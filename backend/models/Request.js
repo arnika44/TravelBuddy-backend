553805f5c141084e59171c40fe54b839aec8f6f5
arnika44 <arnika2135@gmail.com>
2026-07-21 13:29:02 +0530

    Added Request model

---
+++ backend/models/Request.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const requestSchema = new mongoose.Schema({
+  senderPhone: {
+    type: String,
+    required: true,
+  },
+
+  receiverPhone: {
+    type: String,
+    required: true,
+  },
+
+  status: {
+    type: String,
+    default: "Pending",
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Request", requestSchema);
