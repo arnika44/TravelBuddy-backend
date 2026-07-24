06f5857e78165e5fafca050088107c2ff53ed155
arnika44 <arnika2135@gmail.com>
2026-07-24 13:49:59 +0530

    Fix Message model

--- backend/models/Message.js
+++ backend/models/Message.js
@@ -1,11 +1,23 @@
 const mongoose = require("mongoose");
 
-const messageSchema = new mongoose.Schema({
-  sender: String,
-  receiver: String,
-  message: String
-}, {
-  timestamps: true
-});
+const messageSchema = new mongoose.Schema(
+  {
+    senderPhone: {
+      type: String,
+      required: true,
+    },
+    receiverPhone: {
+      type: String,
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("Message", messageSchema);
